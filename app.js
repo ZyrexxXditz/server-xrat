b24ad33b88eca74db6f65b9f5a13df6ef68dfbc3
ZyrexxXditz <zyrex6613@gmail.com>
2024-10-07 21:06:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     apiKey: "AIzaSyCcao7E7qj1mVcAN8LQKRGwr2UHxhC46Q8",
     authDomain: "ditzxratt.firebaseapp.com",
     databaseURL: "https://ditzxratt-default-rtdb.firebaseio.com",
-    projectId: "PROJECT ID",
+    projectId: "ditzxratt",
     storageBucket: "ditzxratt.appspot.com",
     messagingSenderId: "885158369755",
     appId: "1:885158369755:web:49edbac1e92fcf13d02ffe"
