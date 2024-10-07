8f49995d4f24db1162947ad1d3576f3f684a8f88
ZyrexxXditz <zyrex6613@gmail.com>
2024-10-07 21:02:31 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+// Powered By DitzzXploit
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyCcao7E7qj1mVcAN8LQKRGwr2UHxhC46Q8",
+    authDomain: "ditzxratt.firebaseapp.com",
+    databaseURL: "https://ditzxratt-default-rtdb.firebaseio.com",
+    projectId: "PROJECT ID",
+    storageBucket: "ditzxratt.appspot.com",
+    messagingSenderId: "885158369755",
+    appId: "1:885158369755:web:49edbac1e92fcf13d02ffe"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
